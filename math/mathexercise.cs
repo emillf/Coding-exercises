7094b68d5179d166988a315548354f501826bb65
emillf <emillysholtfabergmail.com>
2025-02-07 17:41:05 +0100

    Math exercise

---
+++ math/mathexercise.cs
@@ -0,0 +1,12 @@
+using System;
+class mathvals{
+	static int Main(){
+		public static double sqrt2=Math.sqrt(2.0);
+		public static double fifthroot2=Math.pow(2.0,0.20);
+		public static double E^PI=Math.pow(Math.E,Math.PI);
+		public static double PI^E=Math.pow(Math.PI,Math.E);
+        	Console.WriteLine($"Sqrt(2)={sqrt2}, 2^(1/5) = {fifthroot2}, e^pi={E^PI}, pi^e={PI^E}")
+        	return 0;
+        }
+}
+
