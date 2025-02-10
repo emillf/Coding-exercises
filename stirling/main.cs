4a324d1c77f4eb8a86ae7d139aaeb3cd73b5dcc3
emillf <emillysholtfabergmail.com>
2025-02-10 19:58:24 +0100

    added gamma(10)

--- stirling/main.cs
+++ stirling/main.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 
 class main{
 public static int Main(){
-for(double x=1;x<10;x+=1){
+for(double x=1;x<=10;x+=1){
 	Console.Write($"fgamma({x})={sfuns.fgamma(x)}    ");
 	}
 return 0;
